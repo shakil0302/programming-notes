05aa88cafeef8daa42df6d84416c691a56f4782d
Shakil Ahamed <0day20hour@gtmail.com>
2017-06-08 16:14:22 +0600

    basic cpp code template

---
+++ template/basics.cpp
@@ -0,0 +1,33 @@
+/*
+    ** 1 second == 1/3600 hours.
+*/
+#include <cstdio>
+#include <cassert>
+#include <climits>
+#include <cstring>
+#include <cmath>
+#include <cctype>
+#include <algorithm>
+#include <vector>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+using namespace std;
+//input
+//debug
+//constants + types
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+//helper macros + algorithm
+#define all(x) (x).begin(),(x).end()
+#define sz(z) ((int)(x).size())
+//containers
+#define pb push_back
+#define mk make_pair
+#define fi first
+#define se second
+
+//get started
